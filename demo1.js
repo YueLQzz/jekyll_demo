159b50da98a1515519e4af79163d8dc720778e9a
YueLQzz <yuelqzz@YueLQzztekiMacBook-Pro.local>
2013-09-08 19:46:16 +0800

    onmousemove

--- demo1.js
+++ demo1.js
@@ -1,4 +1,8 @@
-var cx=event.clientX;
-var cy=event.clientY;
-var str="clientX="+cx+",clientY="+cy;
-window.status=str;
+function startup() {
+	var one=document.getElementById("one");
+	var cx=event.clientX;
+	var cy=event.clientY;
+	var str="clientX="+cx+",clientY="+cy;
+	window.status=str;
+	one.innerText=str;
+	}
