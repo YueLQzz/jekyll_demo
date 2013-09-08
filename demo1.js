48a0c98f903f8129ba9c7e74c086fa23777f9169
YueLQzz <yuelqzz@YueLQzztekiMacBook-Pro.local>
2013-09-08 20:32:35 +0800

    firefox  ie  chrome:event

--- demo1.js
+++ demo1.js
@@ -1,8 +1,9 @@
 function startup() {
+	var ev= e || window.event;
 	var one=document.getElementById("one");
-	var cx=event.clientX;
-	var cy=event.clientY;
+	var cx=e.clientX;
+	var cy=e.clientY;
 	var str="clientX="+cx+",clientY="+cy;
 	window.status=str;
-	one.innerText=str;
+	one.innerHTML=str;
 	}
